8589e1d085ac2091ee9e23317cb9fea437485422
Marjovic <151541299+Marjovic@users.noreply.github.com>
2024-08-21 12:10:02 +0800

    Update
    Methods

--- LaboratoryExercise.cpp
+++ LaboratoryExercise.cpp
@@ -1,42 +1,55 @@
 #include <iostream>
 #include <direct.h>;
-#include <dos.h>;
+
 using namespace std;
 
+void list_file();
+void create_directory();
+void change_directory();
+
+
 int main() {
+	
 	int choice;
 	
-	cout << "\t MAIN MENU \n";
-	cout << "------------------------------ \n";
-	cout << "1. To Display List of Files \n";
-	cout << "2. To Create New Directory \n";
-	cout << "3. To Change the Working Directory \n";
-	cout << "4. Exit\n";
-	cout << "Enter the Number: ";
-	cin >> choice;
-	
-	
-	switch (choice) {
+	while (true) {
+		cout << "\t MAIN MENU \n";
+		cout << "------------------------------ \n";
+		cout << "1. To Display List of Files \n";
+		cout << "2. To Create New Directory \n";
+		cout << "3. To Change the Working Directory \n";
+		cout << "4. Exit\n";
+		cout << "Enter the Number: ";
+		cin >> choice;
+		
+		switch (choice) {
 		case 1: 
-			int myChoice;
-			
-			cout << "\n\t LIST FILE DETAIL \n";
-			cout << "------------------------------ \n";
-			cout << "1. List All Files \n";
-			cout << "2. List of Extension Files \n";
-			cout << "3. List of Name Wise \n";	
-			cout << "Enter the Number: ";
-			cin >> myChoice;
+			list_file();
 			break;
 		case 2:
 			break;
 		case 3:
 			break;
 		case 4:
-			cout << "\nThanks For Using!";
+			cout << "\nThanks For Using! \n";
 			return 0;
 		default:
-			cout << "\nInvalid Input!";					
-	}
+			cout << "\nInvalid Input! \n";					
+		}	
+	}	
 	return 0;	
 }
+
+void list_file() {
+	int myChoice;
+	cout << "\n\t LIST FILE DETAIL \n";
+	cout << "------------------------------ \n";
+	cout << "1. List All Files \n";
+	cout << "2. List of Extension Files \n";
+	cout << "3. List of Name Wise \n";	
+	cout << "Enter the Number: ";
+	cin >> myChoice;
+}
+
+
+
